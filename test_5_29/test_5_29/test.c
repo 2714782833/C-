4748ca318b70441992185213999e9e1dd10ef77c
2714782833 <88490919+2714782833@users.noreply.github.com>
2022-05-29 13:08:34 +0800

    Add files via upload

---
+++ test_5_29/test_5_29/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//int sum(int a = 10, int b = 20,int c)
+//{
+//	return a + b + c;
+//}
+//int main()
+//{
+//	int ret = sum(10);
+//	return 0;
+//}
